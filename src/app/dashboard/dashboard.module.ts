a7c64c674b479fcb47dc4dec0de1fd1e5bc085d2
Pawarut Klaiarmon <pawarut.klaiarmon@allianz.com>
2021-10-03 00:59:30 +0700

    Add dashboard component

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { DashboardComponent } from "./pages/dashboard.component";
+
+@NgModule({
+    declarations: [
+        DashboardComponent
+    ],
+    imports: [],
+    providers: []
+})
+export class DashboardModule {};
